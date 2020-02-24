4c1d06497ec510614ca94475c44fe6e773a4d455
melkerlilja <melker.c.lilja@gmail.com>
2020-02-24 19:35:42 +0100

    Initial Commit

---
+++ SktnProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SktnProject
+//
+//  Created by Melker Lilja on 2020-02-24.
+//  Copyright © 2020 Melker Lilja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
